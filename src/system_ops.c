076addbff47306e3be4475d5e25ef67aa5bd50ab
orynX <kahlidalshpli@outlook.com>
2026-03-17 01:11:44 +0100

    save changes

--- src/system_ops.c
+++ src/system_ops.c
@@ -2,18 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
+#include <dirent.h>
 
 #include "system_ops.h"
 #include "ui.h"
+#define MAX_LEN 300
+char command[MAX_LEN];
+ int result;
 
 // this is for update command START
 int handle_update(void)
 {
 
-    char command[300];
     print_status("Starting system update...", 0);
     strcpy(command, "sudo apt update && sudo apt full-upgrade -y");
-    int result = system(command);
+    result = system(command);
     if (result != 0)
     {
         print_status("System update failed.\n", 1);
@@ -29,42 +32,19 @@ int handle_update(void)
 int handle_clean(void)
 {
     print_mode("clean");
-
-    system("df -h");
+    strcpy(command, "sudo apt clean && sudo apt autoclean && sudo apt autoremove -y");
+    result = system(command);
+    if (result != 0)
+    {
+        printf("erorr");
+        return 1;
+    }
+    
 
     print_status("Cleaning apt cache", 0);
-    system("sudo apt clean");
-
     print_status("Running apt autoclean", 0);
-    system("sudo apt autoclean");
-
     print_status("Removing unused packages", 0);
-    system("sudo apt autoremove -y");
-
-    print_status("Clearing user cache", 0);
-    system("rm -rf ~/.cache/*");
-
-    print_status("Clearing thumbnails", 0);
-    system("rm -rf ~/.cache/thumbnails/*");
-
-    print_status("Cleaning logs", 0);
-    system("sudo journalctl --vacuum-time=7d");
-
-    print_status("Clearing temp files", 0);
-    system("sudo rm -rf /tmp/*");
-
-    print_status("Clearing var temp", 0);
-    system("sudo rm -rf /var/tmp/*");
-
-    print_status("Removing crash reports", 0);
-    system("sudo rm -rf /var/crash/*");
-
-    print_status("Cleaning apt partial packages", 0);
-    system("sudo rm -rf /var/cache/apt/archives/partial/*");
-
     printf("\nSystem cleaned successfully.\n\n");
-
-    system("df -h");
 
     return 0;
 }
@@ -72,6 +52,17 @@ int handle_clean(void)
 int handle_ls(void)
 {
     print_mode("ls");
+
+ 
+
+    /*    DIR *dir = opendir(".");
+    struct dirent *entry;
+    int index = 1;
+    if (dir == NULL)
+    {
+        print_status("Cannot open directory.", 1);
+        return 1;
+    } */
 
     return 0;
 }
