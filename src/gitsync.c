af9d2563f7f519878c2b70baab2072908d007c99
orynX <kahlidalshpli@outlook.com>
2026-03-25 17:09:34 +0100

    update

--- src/gitsync.c
+++ src/gitsync.c
@@ -42,10 +42,10 @@ char command[MAX_LEN];
         {
             return 1;
         }
- /*         if (run_system("Pushing changes"C_RESET, "git push") != 0)
+         if (run_system("Pushing changes"C_RESET, "git push") != 0)
         {
             return 1;
-        } */
+        }
 
         print_status("Git sync completed successfully", 0);
 
